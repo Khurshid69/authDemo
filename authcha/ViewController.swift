a4457030edccf4b135583b10cbbe682cf1c74848
khurshid <khurshidrakhimov06@gmail.com>
2021-11-18 01:29:19 +0500

    can approves only Email at this time.

--- authcha/ViewController.swift
+++ authcha/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseEmailAuthUI
+import FirebaseAuthUI
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapLogin(_ sender: Any) {
+        
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            return
+        }
+        
+        authUI?.delegate = self
+        authUI?.providers = [FUIEmailAuth()]
+        
+        
+        let authViewController = authUI?.authViewController()
+        
+        present(authViewController!, animated: true, completion: nil)
+        
+    }
+    
 }
 
+extension ViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if error != nil {
+            return
+        }
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+}
